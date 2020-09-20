48a142e142a6481b5db2fd93c4aefebb172a68db
Наталия Караваева <karavaeva502@gmail.com>
2020-09-20 15:51:16 +0800

    Initial Commit

---
+++ Skillbox 14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skillbox 14
+//
+//  Created by Nataliia on 20.09.2020.
+//  Copyright © 2020 Natalia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
